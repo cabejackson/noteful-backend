bf821d5a0451a825d457efb4e3a7db679ee376ca
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-10-15 23:10:34 -0400

    alter column

--- migrations/003.do.alter_column.sql
+++ migrations/003.do.alter_column.sql
@@ -1,4 +1,4 @@
 ALTER TABLE notes
   ADD COLUMN
     folder_id INTEGER REFERENCES folders(id)
-    ON DELETE SET NULL;-- removed SET NULL should add back
+    ON DELETE CASCADE NOT NULL;;-- removed SET NULL should add back
